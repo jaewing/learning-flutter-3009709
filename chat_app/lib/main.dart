b5119e6c7c137899c87c3485d8ff81a102a6b9ae
ewingjames <162928728+ewingjames@users.noreply.github.com>
2024-12-30 10:17:40 -0800

    Created ChatApp Stateless Widget (stlw) that calls LoginPage stlw for its "home" parameter.

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -9,10 +9,44 @@ void main() {
 
 // Declare a class called "ChatApp" that extends the StatelessWidget class.
 class ChatApp extends StatelessWidget{
+  /// New way using Dart v2.17 or higher.
+  //const ChatApp({super.key});
+  /// Old way.
+  const ChatApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw Text('Hello World!');
+    return MaterialApp(
+      title: "Flutter Chat App!!",
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: LoginPage(),
+    );
   }
+}
 
+class LoginPage extends StatelessWidget {
+  /// This is called the "super-parameters" language feature.
+  ///
+  /// It is the new way of accomplishing what is shown in LIL videos.
+  /// New way using Dart v2.17 or higher.
+  //const LoginPage({super.key});
+
+  /// Old way.
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        //backgroundColor: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button clicked');
+        },
+      ),
+      body: Text('Hello!'),
+    );
+  }
 }
