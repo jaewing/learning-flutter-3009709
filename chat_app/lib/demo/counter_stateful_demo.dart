c2a47a5fdc35bd6865ddae3e3200c49326ef3871
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-01-09 19:54:56 -0800

    Implemented Life cycle methods and monitoring the "mounted" property of State class.

--- chat_app/lib/demo/counter_stateful_demo.dart
+++ chat_app/lib/demo/counter_stateful_demo.dart
@@ -8,19 +8,54 @@ class CounterStateful extends StatefulWidget {
 
   @override
   State<CounterStateful> createState() => _CounterStatefulState();
+  /// Above and below accomplish same result.
+  /*
+  State<CounterStateful> createState () {
+    State<CounterStateful> stateClassAssociatedWithCounterStateful =
+    _CounterStatefulState();
+    return stateClassAssociatedWithCounterStateful;
+  }
+  */
 }
 
 class _CounterStatefulState extends State<CounterStateful> {
   int counter = 0;
 
+
   void increment() {
-    setState(() {
-      counter++;
-    });
+    /// Makes sure you are NOT calling
+    /// setState on an "unmounted" State.
+    /// Avoids runtime crashes.
+    if (mounted) {
+      setState(() {
+        counter++;
+      });
+    }
     print(counter);
+  }
+  @override
+  /// Life cycle methods
+  ///
+  /// First method called.
+  /// Called when widget is created.
+  void initState(){
+    // TODO: implement initState
+    super.initState();
+  }
+  /// Last method called.
+  /// Called when widget is destroyed.
+  void dispose(){
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
+  /// Once BuildContext is associated with State,
+  /// the property "mounted" of State will be
+  /// assigned to "True".
+  ///
+  /// Allows you to execute something only after
+  /// the State object is successfully created.
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
