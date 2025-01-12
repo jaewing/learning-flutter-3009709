2ed3d432889ed3872d7d602d952a6334c2767ee2
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-01-11 18:48:12 -0800

    Implemented the concept of capturing touch screen taps from the user. The two classes associated with this are InkWell and GestureDetector.

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -35,7 +35,7 @@ class LoginPage extends StatelessWidget {
                     color: Colors.blueGrey),
               ),
               Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHp1Ymo4YnV4ejJ4ZnF5em80NGpxanRqZm1lbG5lbTVjNnp0ZW90aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l3q2zVr6cu95nF6O4/giphy.webp',
                 height: 200,
               ),
               ElevatedButton(
@@ -45,11 +45,36 @@ class LoginPage extends StatelessWidget {
                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
                   )),
               //TODO: Add the supporting text by final design
-              TextButton(
-                  onPressed: () {
-                    print('Pressed on the URL!');
-                  },
-                  child: Text('https://poojabhaumik.com'))
+              /// Tap and Gesture detection.
+              /// Widgets: GestureDetector and InkWell
+              ///
+              /// Both Text and the Text URL Button should
+              /// both react to a user "tap".
+              ///
+              /// We accomplish this via wrapping both Text widgets with a
+              /// Column and then a GestureDetector widget.
+              ///
+              /// InkWell gives you the "splash" effect of a button being
+              /// tapped to give user feedback they tapped the right button.
+              InkWell(
+                splashColor: Colors.grey,
+                onLongPress: (){
+                  print('long pressed!');
+                },
+                onDoubleTap: (){
+                  print('double tapped!');
+                },
+                onTap: (){
+                  //todo: Navigate to browser
+                  print('Link clicked!');
+                },
+                child: Column(
+                  children: [
+                    Text('Find us on'),
+                    Text('https://poojabhaumik.com'),
+                  ],
+                ),
+              )
             ],
           ),
         ),
