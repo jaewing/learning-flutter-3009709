30612fb983b2c70a2767fff9088d79623caef677
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-01-11 18:07:15 -0800

    Implemented two of the three Button types available in Flutter.

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -3,40 +3,74 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  void loginUser() {
+    print('login successful!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final themeColor = Theme.of(context).primaryColor;
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
 
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHp1Ymo4YnV4ejJ4ZnF5em80NGpxanRqZm1lbG5lbTVjNnp0ZW90aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l3q2zVr6cu95nF6O4/giphy.webp',
+                height: 200,
+              ),
 
-            //TODO: Add Login Button
+              /// Three types of Buttons.
+              /// ElevatedButton
+              //TODOne: Add Login Button
+              ElevatedButton(
+                  /// If we assign "onPressed: null" it would disable
+                  /// the Button.
+                  ///
+                  /// In future the button state should depend on the
+                  /// "Form" widgets.
+                  ///
+                  /// onPressed requires a "void" type function.
+                  onPressed: loginUser,
+                  child: Text(
+                    'Click me!',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                  )),
 
-            //TODO: Add URL Text Button
-          ],
+              /// OutlinedButton
+              //OutlinedButton(onPressed: (){}, child: FlutterLogo()),
+              /// TextButton
+
+              //TODOne: Add URL Text Button
+              TextButton(
+                  onPressed: () {
+                    print('Pressed on the URL!');
+                  },
+                  child: Text('https://poojabhaumik.com')
+              ),
+            ],
+          ),
         ),
       ),
     );
--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -9,8 +9,23 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Chat App",
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      /// primarySwatch gradually being phased out by Flutter devs.
+      /// Use colorScheme instead.
+      theme: ThemeData(
+          colorScheme: ColorScheme(
+              brightness: Brightness.dark,
+              primary: Colors.deepPurple,
+              onPrimary: Colors.black,
+              secondary: Colors.purple,
+              onSecondary: Colors.black,
+              error: Colors.red,
+              onError: Colors.white,
+              surface: Colors.white38,
+              onSurface: Colors.black,
+          ),
+      ),
       home: LoginPage(),
     );
   }
