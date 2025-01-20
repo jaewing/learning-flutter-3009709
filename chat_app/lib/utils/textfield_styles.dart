642cd8ab561f6f27c05ba0cbd22b6b07663df986
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-01-19 17:51:19 -0800

    Implemented the concept of adding an "assets" folder to your Flutter project to contain any files you want associated with your project. We associate the assets to the project under the "assets" property of the pubspec.yaml file. Used an image as an example and placed it in the assets folder and displayed it with the "Image.asset" method.

--- chat_app/lib/utils/textfield_styles.dart
+++ chat_app/lib/utils/textfield_styles.dart
@@ -1,6 +1,14 @@
-
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class ThemeTextStyle{
-  static TextStyle loginTextFieldStyle = TextStyle(color: Colors.blueGrey);
+  /// Here we use the GoogleFonts class to set
+  /// the font of the loginTextFieldStyle.
+  ///
+  /// In this case we choose the "lato" font.
+  static TextStyle loginTextFieldStyle = GoogleFonts.lato(
+      textStyle:  TextStyle(
+          color: Colors.blueGrey
+      ),
+  );
 }
