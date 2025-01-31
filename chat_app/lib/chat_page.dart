e572fcadf2f5a7f1ef2442f7a23805dd9a4ae995
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-01-30 18:52:47 -0800

    Implemented the concept of importing data from JSON files. This is the concept of JSON "Deserialization". The "chat_message_entity.dart" file contains a class that is representative of the JSON message structure. We then import the messages from the JSON file after decoding them within "chat_page.dart" in the "_loadInitialMessages" method.

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,33 +1,64 @@
+import 'dart:convert';
+
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
 
-  List<ChatMessageEntity> _messages = [
-    ChatMessageEntity(
-        author: Author(userName: 'pooja'),
-        createdAt: 2131231242,
-        id: '1',
-        text: 'First text'),
-    ChatMessageEntity(
-        author: Author(userName: 'pooja'),
-        createdAt: 2131231442,
-        id: '1',
-        text: 'Second text',
-        imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
-    ChatMessageEntity(
-      author: Author(userName: 'jane'),
-      createdAt: 2131234242,
-      id: '1',
-      text: 'Third text',
-    )
-  ];
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+
+  /// Here we create a list that holds all the messages we are pulling from
+  /// JSON file.
+  List<ChatMessageEntity> _messages = [];
+
+  /// This method waits for our mock messages JSON file is loaded in as
+  /// a String. We then take that String and decode it as a List. Then
+  /// we structure that List with the "map" method called on decodedList
+  /// and make a new List containing only ChatMessageEntity type items. We
+  /// then return that ChatMessageEntity type List. Following this, we call
+  /// setState and assign our imported JSON _chatMessages to our private
+  /// class property _messages.
+  _loadInitialMessages() async{
+    final response = await rootBundle.loadString('assets/mock_messages.json');
+
+    /// We specify concrete type check of List and cast the jsonDecode
+    /// return value as a "List" to make sure runtime error is caught
+    /// if the decoded response is NOT a list.
+    final List<dynamic>decodedList = jsonDecode(response) as List;
+
+    /// _chatMessages will contain a list of "ChatMessageEntity" type objects.
+    /// "map" method lets you expose each item in the decodedList as "listItem"
+    /// and iterate through the "listItem"s.
+    final List<ChatMessageEntity> _chatMessages= decodedList.map((listItem){
+      return ChatMessageEntity.fromJson(listItem);
+
+    }).toList();
+    print(_chatMessages.length);
+
+    /// Final sate of the messages.
+    setState((){
+      _messages = _chatMessages;
+    });
+  }
+
+  /// Call our initialize method.
+  void initState(){
+    _loadInitialMessages();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    /// We now call this in our initState() method.
+    //_loadInitialMessages();
     final username = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
@@ -51,10 +82,11 @@ class ChatPage extends StatelessWidget {
                   itemCount: _messages.length,
                   itemBuilder: (context, index) {
                     return ChatBubble(
-                        alignment: _messages[index].author.userName == 'pooja'
+                        alignment: _messages[index].author.userName == 'poojab26'
                             ? Alignment.centerRight
                             : Alignment.centerLeft,
-                        entity: _messages[index]);
+                        entity: _messages[index]
+                    );
                   })),
           ChatInput(),
         ],
--- chat_app/lib/models/chat_message_entity.dart
+++ chat_app/lib/models/chat_message_entity.dart
@@ -11,10 +11,28 @@ class ChatMessageEntity {
       required this.createdAt,
       this.imageUrl,
       required this.author});
+
+  factory ChatMessageEntity.fromJson(Map<String, dynamic> json){
+    return ChatMessageEntity(
+        text: json['text'],
+        id: json['id'],
+        createdAt: json['createdAt'],
+        /// Author is a class within a class, which
+        /// is why we handle this differently.
+        /// Matches structure of JSON file.
+        author: Author.fromJson(json['author'])
+    );
+  }
 }
 
 class Author {
   String userName;
 
   Author({required this.userName});
+
+  /// Here we create a named constructor. "factory"
+  /// keyword, I am uncertain the importance of.
+  factory Author.fromJson(Map<String, dynamic> json){
+    return Author(userName: json['username']);
+  }
 }
