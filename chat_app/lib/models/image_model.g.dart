b5d7c9d78caaad17e7e3f600f82422e8047b87ee
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-02-01 14:54:55 -0800

    Implemented the concept setting up model for Json interaction to use the JsonSerializable package. This package allows you to generated a dart file that contains the Serialize and Deserialize methods for JSON interaction. Prevents you from having to manually map all of your Dart properties to the JSON string variables.

---
+++ chat_app/lib/models/image_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GirlImage _$GirlImageFromJson(Map<String, dynamic> json) => GirlImage(
+      description: json['description'] as String,
+      imageUrl: json['image-url'] as String,
+    );
+
+Map<String, dynamic> _$GirlImageToJson(GirlImage instance) => <String, dynamic>{
+      'description': instance.description,
+      'image-url': instance.imageUrl,
+    };
