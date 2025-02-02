db338c2b95d48eeba5de0efdeb0dcc6ce8c9a81a
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-02-01 16:54:36 -0800

    Implemented the concept of deserialization from a remote URL that hosts a JSON file. This required importing the "http" package with an alias of "http". We then create an "async" method to parse our URL with the Uri class and then use the "http.get" method paired with the "await" keyword to pull in the HTTP response. We then check the property "statusCode" of response to make sure its == 200, and NOT an error of something like 404. If condition passes, we then decode the JSON file with type "Map<String, dynamic>" and then access the "GirlImages" key of that Map and cast it as a List type. We then cast a concrete type of List<GirlImage? onto _imageList and assign it to the decodedList extracted with the "map" method which iterates through the decodedList until it has "finally" assigned all list items to _imageList. To utilize this whole method, we then call it within our "build" method.

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
-
+import 'package:chat_app/models/image_model.dart';
+/// Make sure to specify alias as "http" for the
+/// package reference.
+import 'package:http/http.dart' as http;
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
@@ -39,17 +42,71 @@ class _ChatPageState extends State<ChatPage> {
     setState(() {});
   }
 
-  //TODO: Get Network Images from API
-  _getNetworkImages() {}
+  //TODOne: Get Network Images from API
+  /// Here we will connect to the API that we
+  /// were testing on Postman.
+  _getNetworkImages() async{
+    var endpointUrl = Uri.parse('https://gist.githubusercontent.com/hiteshsahu/f58bcca95532fde77fd0d9e94a9c3148/raw/4ef7b30240c781341f1994f12453e9e7a5c2c67d/GirlImages.json7');
+
+    ///Need to add "http" package to utilize this method.
+    ///Also make sure to specify "as http" at the end to
+    ///create alias to the package. We do this to avoid
+    ///confusion with core Dart "get" functionality.
+    ///
+    /// Need to do this asynchronously with "async" paired with "await".
+    ///
+    /// Now we replicate what we did with handling ChatMessageEntity by
+    /// decoding the JSON into a list of String<GirlImage> objects.
+    ///
+    /// 200 Response statusCode is "OK"
+    /// 404 Response statusCode is "Error"
+    final response = await http.get(endpointUrl);
+
+    /// Check to make sure our statusCode
+    /// is giving us "OK" before proceeding.
+    if(response.statusCode == 200){
+      /// Below is how this would be implemented if the JSON was purely
+      /// of "List" formatting. For the URL I am pulling from, its a little
+      /// different as it is in Map formatting.
+      /*
+    final List<dynamic> decodedList = jsonDecode(response.body) as List;
+
+    final List<GirlImage> _chatMessages = decodedList.map((listItem) {
+      return GirlImage.fromJson(listItem);
+    }).toList();
+    */
+      /// Here we decode the JSON and access the "GirlImages" key.
+      final Map<String, dynamic> decodedResponse = jsonDecode(response.body) as Map<String, dynamic>;
+      final List<dynamic> decodedList = decodedResponse['GirlImages'] as List;
+
+      /// Here we map each item in the list to a GirlImage object.
+      final List<GirlImage> _imageList = decodedList.map((listItem){
+        return GirlImage.fromJson(listItem);
+      }).toList();
+
+      /// "body" will give you the JSON response.
+      print(_imageList[0].imageUrl);
+    }
+
+    else{
+      print('404 Not Found!');
+    }
+
+  }
 
   @override
   void initState() {
     _loadInitialMessages();
+    /// Here we call our API method.
+    //_getNetworkImages();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    /// We add our API method here so we
+    /// can do hot reload.
+    _getNetworkImages();
     final username = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
