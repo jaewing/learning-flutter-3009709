a0321551f77ff9cdd812d0a78ec3684618843c94
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-02-16 16:07:30 -0800

    Implemented additional functionality for the image selection window including displaying the image in the chat entry field before being sent along with text. The image selection window closes after user image selection. Send button clears the state of the chat entry field and appends new chat bubble with image and text (checks if image is blank before appending).

--- chat_app/lib/widgets/chat_input.dart
+++ chat_app/lib/widgets/chat_input.dart
@@ -2,10 +2,19 @@ import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/picker_body.dart';
 import 'package:flutter/material.dart';
 
-class ChatInput extends StatelessWidget {
+class ChatInput extends StatefulWidget {
   final Function(ChatMessageEntity) onSubmit;
 
   ChatInput({Key? key, required this.onSubmit}) : super(key: key);
+
+  @override
+  State<ChatInput> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  /// Default state is empty since most cases we
+  /// will not have an image to attach.
+  String _selectedImageUrl = '';
 
   final chatMessageController = TextEditingController();
 
@@ -17,14 +26,40 @@ class ChatInput extends StatelessWidget {
         createdAt: DateTime.now().millisecondsSinceEpoch,
         author: Author(userName: 'poojab26'));
 
+    /// Check to make sure user selected image.
+    if(_selectedImageUrl.isNotEmpty)
+          newChatMessage.imageUrl = _selectedImageUrl;
+
     //TODO: Check for image, and append it to the chat object
-    onSubmit(newChatMessage);
+    widget.onSubmit(newChatMessage);
+
+      /// Clears state of the Chat input box.
+      chatMessageController.clear();
+      /// Set the selected Image Url back to empty string.
+      _selectedImageUrl = '';
+      setState(() {});
+  }
+
+  /// Function we pass to the "onImageSelected" property
+  /// in child widget "picker_body".
+  void onImagePicked(String newImageUrl){
+    setState(() {
+      /// newImageUrl passed by child "picker_body".
+      _selectedImageUrl = newImageUrl;
+    });
+
+    /// Closes the select image window after
+    /// you selected the image you want to
+    /// send.
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
+      /// By removing the height constraint here,
+      /// we allow the Container to resize dynamically.
+      //height: MediaQuery.of(context).size.height * 0.25,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -33,7 +68,8 @@ class ChatInput extends StatelessWidget {
               showModalBottomSheet(
                   context: context,
                   builder: (BuildContext context) {
-                    return NetworkImagePickerBody();
+                    return NetworkImagePickerBody(
+                      onImageSelected: onImagePicked,);
                   });
             },
             icon: Icon(
@@ -42,18 +78,27 @@ class ChatInput extends StatelessWidget {
             ),
           ),
           Expanded(
-              child: TextField(
-            keyboardType: TextInputType.multiline,
-            maxLines: 5,
-            minLines: 1,
-            controller: chatMessageController,
-            textCapitalization: TextCapitalization.sentences,
-            style: TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-                hintText: "Type your message",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                border: InputBorder.none),
-          )),
+              /// Here we wrap TextField with Column widget
+              /// to allow us to place an image widget along
+              /// the vertical axis with the text.
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              minLines: 1,
+              controller: chatMessageController,
+              textCapitalization: TextCapitalization.sentences,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  hintText: "Type your message",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  border: InputBorder.none),
+            ),
+                if (_selectedImageUrl.isNotEmpty)
+                  Image.network(_selectedImageUrl, height: 200),
+              ])),
           IconButton(
             onPressed: onSendButtonPressed,
             icon: Icon(
--- chat_app/lib/widgets/picker_body.dart
+++ chat_app/lib/widgets/picker_body.dart
@@ -3,24 +3,45 @@ import 'package:chat_app/repo/image_repository.dart';
 import 'package:flutter/material.dart';
 
 class NetworkImagePickerBody extends StatelessWidget {
+
+  /// Property in which we will pass the image returned
+  /// by our onTap callback to our parent widget (chat_input).
+  final Function(String) onImageSelected;
   NetworkImagePickerBody({
     Key? key,
+    required this.onImageSelected,
   }) : super(key: key);
 
   final ImageRepository _imageRepo = ImageRepository();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<PixelfordImage>>(
+    return FutureBuilder<List<GirlImage>>(
         future: _imageRepo.getNetworkImages(),
         builder: (BuildContext context,
-            AsyncSnapshot<List<PixelfordImage>> snapshot) {
+            AsyncSnapshot<List<GirlImage>> snapshot) {
           if (snapshot.hasData) {
             return GridView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 //TODO: Add a Gesture Detector to register clicks on each image
-                return Image.network(snapshot.data![index].urlSmallSize);
+                /// We are going to add the ability to add the Image to chat
+                /// message we want to send.
+                /// We do this via wrapping our image with a GestureDetector widget.
+                return GestureDetector(
+                  /// Listens to any tap events and returns
+                  /// a callback.
+                  /// We provide a callback that will fetch the image
+                  /// and send it back to our parent widget (chat_input)
+                  /// so we can display it in the chat message widget.
+                  ///
+                  /// Important: passing image from child to parent widget
+                  onTap: (){
+                    onImageSelected(snapshot.data![index].imageUrl);
+
+                  },
+                    child: Image.network(snapshot.data![index].imageUrl)
+                );
               },
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                   crossAxisSpacing: 2,
