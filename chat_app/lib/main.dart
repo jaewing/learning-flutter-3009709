3ac17558f301b17161a7855fa61056929b652b75
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-02-23 12:17:18 -0800

    Implemented the concept of utilizing a "static" initializer. Static properties and methods are independent of class instances/objects. Meaning, static properties/methods can exist without having to create an instance of a class. We implement this concept via adding a static "init" method to our "AuthService" class and then calling this method in our main loop with the addition of an "async" on the main loop and "await" for our static "init" method. We call our static "init" method before MaterialApp because it has to be called before we render our first widget aka "MaterialApp".

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,8 +4,20 @@ import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+/// Here we define our main() loop
+/// as "async" because we have an
+/// instance of AuthService which
+/// contains the static Future method
+/// "init".
+void main() async{
   //TODO: Integrate Provider for State Management
+  /// Makes sure we have an instance of the
+  /// widgets binding before interacting with the
+  /// native code of the deployed device.
+  WidgetsFlutterBinding.ensureInitialized();
+  /// This is where all your initialization should
+  /// happen before the app is called and rendered.
+  await AuthService.init();
   runApp(Provider(
     create: (BuildContext context) => AuthService(),
     child: ChatApp(),
--- chat_app/lib/services/auth_service.dart
+++ chat_app/lib/services/auth_service.dart
@@ -2,25 +2,56 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
 
-  //TODO: Remove duplicate prefs initialization call
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  /// Static properties or methods are
+  /// independent of objects or instances of
+  /// a class. They can exist without having
+  /// to create an instance of a class.
+  ///
+  /// Static methods also can be "async".
+  ///
+  /// Method has to be called before we render
+  /// our first widget (MaterialApp). So this
+  /// initialization has to happen in "main.dart".
+  ///
+  /// After implementing our static initializers concept
+  /// we are now able to amend the properties of our
+  /// SharedPreferences instance directly without redundant
+  /// instantiation of captured objects.
+  static init() async{
+    _prefs = await SharedPreferences.getInstance();
+  }
 
+  //TODO: Remove duplicate prefs initialization call
+  /// Currently have a lot of duplicity since we are over
+  /// and over again capturing a SharedPreferences instance
+  /// in order to edit the properties of that instance.
+  ///
+  /// Here we define our private property as "late" variable
+  /// because the initialization will happen later.
+  /// Also add "static" because we can NOT have properties or
+  /// methods tied to a class instance being used within our
+  /// static method "init".
+  static late final SharedPreferences _prefs;
+
+  /// Why is the Future<void> and async combo necessary
+  /// here?
+  ///
+  /// There is no "await" that I can tell. "set" method
+  /// definitely is of Future type, but so are the "clear"
+  /// and "get" methods.
   Future<void> loginUser(String userName) async {
     try {
-      SharedPreferences sharedPrefs = await _prefs;
-      sharedPrefs.setString('userName', userName);
+      _prefs.setString('userName', userName);
     } catch (e) {
       print(e);
     }
   }
 
-  void logoutUser() async {
-    SharedPreferences sharedPrefs = await _prefs;
-    sharedPrefs.clear();
+  void logoutUser() {
+    _prefs.clear();
   }
 
-  Future<String?> getUserName() async {
-    SharedPreferences sharedPrefs = await _prefs;
-    return sharedPrefs.getString('userName') ?? 'DefaultValue';
+  String? getUserName(){
+    return  _prefs.getString('userName') ?? 'DefaultValue';
   }
 }
