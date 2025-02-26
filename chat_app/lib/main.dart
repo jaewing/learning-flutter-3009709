812e6d7a3a6c48ab60c4865eaf2cf646d9998ed4
ewingjames <162928728+ewingjames@users.noreply.github.com>
2025-02-25 22:28:21 -0800

    Implemented the concept of checking what device the app is going to be deployed on. If on web, we return a certain layout. If on mobile we return normal layout we have been using. Used the LayoutBuilder widget to accomplish this. LAST LESSON!!!!!!!!!!!!!

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -17,6 +17,7 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Chat App",
       theme: ThemeData(
           canvasColor: Colors.transparent,
